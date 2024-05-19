df51264d77b739a0083071a9d343df73729c5668
Chaitanya Agarwal <53862555+Chaitanya2801@users.noreply.github.com>
2024-05-19 02:20:46 -0700

    Add files via upload
    single number_i leetecode problem 136 solution

---
+++ Bit Masking/Single_Number_I.java
@@ -0,0 +1,15 @@
+package lec23;
+
+public class Single_Number_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {4,1,2,1,2};
+		int a = 0;
+		for (int i = 0; i < arr.length; i++) {
+			a = a^arr[i];
+		}
+		System.out.println(a);
+	}
+
+}
