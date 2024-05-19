c90e1d1bed966afec5e9d233e0a0ee5e2ddca85d
Chaitanya Agarwal <53862555+Chaitanya2801@users.noreply.github.com>
2024-05-19 01:42:44 -0700

    Add files via upload
    fast approach for count set bits using brian kernighan's algorithms making loop iteration limted to number of se bits

--- Bit Masking/Count_Set_Bit.java
+++ Bit Masking/Count_Set_Bit.java
@@ -6,6 +6,7 @@ public class Count_Set_Bit {
 		// TODO Auto-generated method stub
 		int n = 84;
 		System.out.println(CountSetBit(n));
+		System.out.println(fastCount(n));
 	}
 	
 	public static int CountSetBit(int n) {
@@ -15,6 +16,15 @@ public class Count_Set_Bit {
 				c++;
 			}
 			n >>= 1; // right shift works as divisor by 2
+		}
+		return c;
+	}
+	
+	public static int fastCount(int n) { // Brian Kernighan's Algorithms
+		int c = 0;
+		while(n>0) {
+			n = (n & (n - 1));
+			c++;
 		}
 		return c;
 	}
