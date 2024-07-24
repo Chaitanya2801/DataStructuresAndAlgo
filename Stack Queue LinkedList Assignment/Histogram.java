fca2458c2e2769653a3b92d5602cfead9dfd4650
Chaitanya Agarwal <53862555+Chaitanya2801@users.noreply.github.com>
2024-07-24 19:23:00 +0530

    histogram solution

---
+++ Stack Queue LinkedList Assignment/Histogram.java
@@ -0,0 +1,122 @@
+package stack_queue_linkedList_assignment;
+
+import java.util.*;
+public class Histogram {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0;i < n;i++)
+		    arr[i] = s.nextInt();
+		    
+    		Histogram mainobj = new Histogram();
+    		StacksUsingArrays stack = mainobj.new StacksUsingArrays(1000);
+    		System.out.println(hist(arr, stack));
+		}
+	
+
+	public static long hist(int[] arr, StacksUsingArrays st) throws Exception {
+		long ans = 0;
+		for (int i = 0; i < arr.length; i++) {
+			while (!st.isEmpty() && arr[i] < arr[st.top()]) {
+				long h = arr[st.pop()];
+				long r = i;
+				if (st.isEmpty()) {
+					ans = Math.max(ans, h * r);
+				} else {
+					long l = st.top();
+					ans = Math.max(ans, h * (r - l - 1));
+				}
+			}
+			st.push(i);
+		}
+		long r = arr.length;
+		while (!st.isEmpty()) {
+			long h = arr[st.pop()];
+			if (st.isEmpty()) {
+				ans = Math.max(ans, h * r);
+			} else {
+				long l = st.top();
+				ans = Math.max(ans, h * (r - l - 1));
+			}
+		}
+		return ans;
+	    
+	}
+
+	private class StacksUsingArrays {
+		private int[] data;
+		private int tos;
+
+		public static final int DEFAULT_CAPACITY = 10;
+
+		public StacksUsingArrays() throws Exception {
+			// TODO Auto-generated constructor stub
+			this(DEFAULT_CAPACITY);
+		}
+
+		public StacksUsingArrays(int capacity) throws Exception {
+			if (capacity <= 0) {
+				System.out.println("Invalid Capacity");
+			}
+			this.data = new int[capacity];
+			this.tos = -1;
+		}
+
+		public int size() {
+			return this.tos + 1;
+		}
+
+		public boolean isEmpty() {
+			if (this.size() == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public void push(int item) throws Exception {
+			if (this.size() == this.data.length) {
+				throw new Exception("Stack is Full");
+			}
+			this.tos++;
+			this.data[this.tos] = item;
+		}
+
+		public int pop() throws Exception {
+			if (this.size() == 0) {
+				throw new Exception("Stack is Empty");
+			}
+			int retVal = this.data[this.tos];
+			this.data[this.tos] = 0;
+			this.tos--;
+			return retVal;
+		}
+
+		public int top() throws Exception {
+			if (this.size() == 0) {
+				throw new Exception("Stack is Empty");
+			}
+			int retVal = this.data[this.tos];
+			return retVal;
+		}
+
+		public void display() throws Exception {
+			if (this.size() == 0) {
+				throw new Exception("Stack is Empty");
+			}
+			for (int i = this.tos; i >= 0; i--) {
+				System.out.println(this.data[i]);
+			}
+
+		}
+
+	}
+
+}
+
+
